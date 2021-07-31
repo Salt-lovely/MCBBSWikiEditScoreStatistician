4d6112d6ae14956deee298e32201f2b1a42259f5
Salt-lovely <732237136@qq.com>
2021-07-31 14:58:52 +0800

    DOC: 补全注释

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,25 +1,32 @@
 import { chunk } from './utils/chunk';
 import { flat } from './utils/flat';
 
+/** 链接，查询包括用户名、用户id、编辑id等 */
 const url = 'https://mcbbs-wiki.cn/api.php?action=query&format=json&prop=revisions&rvprop=user%7Cids%7Cuserid&revids=';
+/** 出错时返回这个空数据 */
 const badData: BadQueriedData = {
   batchcomplete: '',
   query: {
     badrevids: {},
   },
 };
+
 /**
  * 根据 rid 获取编辑信息
  * @param rid 编辑的 rid
  * @returns 获取到的数据
  */
-
 export async function fetchRid(rid: number): Promise<QueriedData> {
   return await fetch(url + rid)
     .then((res) => res.json())
     .catch(() => badData);
 }
 
+/**
+ * 批量查询
+ * @param rids rid列表
+ * @returns 查询到的数据
+ */
 export async function fetchRids(...rids: number[]): Promise<QueriedData[]> {
   const res: QueriedData[] = [];
   for (let rid of rids) {
@@ -28,6 +35,12 @@ export async function fetchRids(...rids: number[]): Promise<QueriedData[]> {
   return res;
 }
 
+/**
+ * 多进程查询
+ * @param rids rid列表
+ * @param threads 进程数
+ * @returns 查询到的数据
+ */
 export async function mutiThreadFetchRids(rids: number[], threads: number = 4): Promise<QueriedData[]> {
   const ridQueue = chunk(rids, threads);
   const taskQueue: Promise<QueriedData[]>[] = ridQueue.map((rids) => fetchRids(...rids));
