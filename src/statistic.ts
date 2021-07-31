031bc87065e0c8e2f5343957410a31c84bfbe4ea
Salt-lovely <732237136@qq.com>
2021-07-31 16:09:16 +0800

    FEAT: 数据统计工具

---
+++ src/statistic.ts
@@ -0,0 +1,45 @@
+const userIdMap: {
+  [uid: number]: string;
+} = {};
+const idUserMap: {
+  [user: string]: number;
+} = {};
+
+const CachedDatas: CachedDatas = [];
+
+/** 通过 uid 获取用户名 */
+export function getUserName(uid: number): string | undefined {
+  return userIdMap[uid];
+}
+
+/** 通过用户名获取 uid */
+export function getUserId(uname: string): number | undefined {
+  return idUserMap[uname];
+}
+
+/** 缓存获取的数据 */
+export function setData(datas: QueriedData[]) {
+  for (let data of datas) {
+    if (!data.query.pages) continue;
+    const k = Object.keys(data.query.pages)[0];
+    if (!k) continue;
+    const revision = data.query.pages[k].revisions[0];
+    // 只记录名字空间和uid
+    CachedDatas[revision.revid] = {
+      ns: data.query.pages[k].ns,
+      uid: revision.userid,
+    };
+    // 记录 uid 与用户间的映射关系
+    if (!userIdMap[revision.userid]) userIdMap[revision.userid] = revision.user;
+    if (!idUserMap[revision.user]) idUserMap[revision.user] = revision.userid;
+  }
+}
+
+/** 获取某个uid下的所有编辑记录 */
+export function getDataByUid(uid: number): CachedDatas {
+  const res: CachedDatas = [];
+  for (let data of CachedDatas) {
+    if (data.uid === uid) res.push(Object.assign({}, data));
+  }
+  return res;
+}
